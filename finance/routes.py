db8ac6143d50bea33f3062d0ee8467d3914b89a7
Slava Kalmykov <slavafort35@gmail.com>
2025-12-06 22:49:58 +0100

    profile route has been created

--- finance/routes.py
+++ finance/routes.py
@@ -1,11 +1,11 @@
 import functools
 
 from flask import render_template, redirect, request,url_for, flash, session
-from finance import app
+from finance import app, db
 from finance.forms import LoginForm, RegistrationForm
 from finance.logger import logger
 from finance.models import User
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 def log_exceptions(func):
@@ -14,10 +14,10 @@ def log_exceptions(func):
         try:
             return func(*args, **kwargs)
         except Exception as e:
-            logger.error(f'Ошибка в {func.__name__}: {str(e)}', exc_info=True)
+            logger.error(f'Error in {func.__name__}: {str(e)}', exc_info=True)
             from flask import request
             if request:
-                return 'Произошла ошибка на сервере', 500
+                return 'An error occurred on the server', 500
             else:
                 raise
 
@@ -46,7 +46,7 @@ def login_user_post():
         nick_name = login_form.nick_name.data
         password = login_form.password.data
 
-        user = User.query.filter_by(personnel_number=nick_name).first()
+        user = User.query.filter_by(nick_name=nick_name).first()
         if not user or not check_password_hash(user.password, password):
             flash('Incorrect login or password', 'danger')
             return redirect(url_for('login_user_get'))
@@ -63,3 +63,57 @@ def login_user_post():
 def register_user_get():
     registration_form = RegistrationForm(request.form)
     return render_template('login_register.html', register_tab=True, registration_form=registration_form)
+
+
+@app.route('/register', methods=['POST'])
+@log_exceptions
+def register_user_post():
+    registration_form = RegistrationForm(request.form)
+
+    if registration_form.validate_on_submit():
+        nick_name = registration_form.nick_name.data
+        password = registration_form.password.data
+
+        # if not validate_register_form(personnel_number, password):
+        #     return render_template('login_register.html', register_tab=True, registration_form=registration_form)
+
+        if User.query.filter_by(nick_name=nick_name).first() is not None:
+            flash('This nickname already exists', 'danger')
+            return render_template('login_register.html', register_tab=True, registration_form=registration_form)
+
+        new_user = User(
+            nick_name=nick_name,
+            password=generate_password_hash(password)
+        )
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+
+            flash('You have successfully registered.', 'success')
+            return redirect(url_for('login_user_get'))
+        except Exception as e:
+            db.session.rollback()
+            flash(f'An error occurred: {str(e)}', 'danger')
+    else:
+        flash('Incorrect registration data', 'danger')
+        return render_template('login_register.html', register_tab=True, registration_form=registration_form)
+
+
+@app.route('/logout')
+@log_exceptions
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('return_main_page'))
+
+
+@app.route('/profile', methods=['GET'])
+@log_exceptions
+def return_profile():
+    user_id = session.get('user_id')
+    if user_id:
+        user = User.query.filter_by(id=user_id).first()
+        return render_template('profile.html', user=user)
+    else:
+        flash('You need to log in', 'danger')
+        return redirect(url_for('login_user_get'))
